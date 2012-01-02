9b3fb3dd21ed749bd2281d0ed945a8641009c22a
Heuristic Fencepost <h.fencepost@gmail.com>
2012-01-01 19:25:33 -0600

    Added test to verify that we can't easily inject actor scope into a closure passed into an Akka actor.  Likely resolution for messages that need send-and-receive semantics (or even send-and-future semantics) is to use a defined class for the actor.

--- src/ruby/tests/proc_actor_test.rb
+++ src/ruby/tests/proc_actor_test.rb
@@ -37,4 +37,35 @@ class ProcActorTest < Test::Unit::TestCase
 
     ref.stop
   end
+
+  # This test currently fails for a known reason... albeit one that I can't resolve yet.  The closure passed
+  # into the actor factory references all free variables to the scope in which it was defiend; that's sort of
+  # the point of a closure.  The problem is that we also need to allow the code within this closure to reference
+  # the actor in which it runs, or at least the context for that actor, in order to use getContext.replySafe from
+  # the Java API.  But at this point in the process the actor isn't accessible to the scope... for that matter the
+  # actor isn't even built yet.
+  def test_proc_actor_with_futures
+
+    # Create a factory for an actor that uses the input block to pass incoming messages.
+    factory = ProcActorFactory.new do |msg|
+      rv = 2 * msg
+      # Fails completely.  self in this code references the unit test scope, but what we need is some way
+      # to refer to the actor's scope.
+      self.getContext.replySafe rv
+    end
+    ref = Actors.actorOf factory
+    ref.start
+
+    sample = rand 100
+    resp = ref.sendRequestReplyFuture sample
+
+    # Give messages time to propagate
+    sleep 3
+
+    resp.await
+    assert resp.isCompleted
+    respval = resp.result
+    assert respval.isDefined
+    assert_equal(2 * sample,respval.get)
+  end
 end
